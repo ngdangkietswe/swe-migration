98c971b33e15c0fda2e3201c05a4afd9e2bead95
ngdangkietse <kietnguyen17052001@gmail.com>
2024-12-31 10:40:07 +0700

    ops(cicd): flyway migration task prod

--- script/migrate_prod.sh
+++ script/migrate_prod.sh
@@ -1,2 +1,5 @@
 echo "Migrating auth database ..."
 mvn flyway:migrate -Dflyway.configFiles=config/prod/flyway_auth.conf
+
+echo "Migrating task database ..."
+mvn flyway:migrate -Dflyway.configFiles=config/prod/flyway_task.conf
