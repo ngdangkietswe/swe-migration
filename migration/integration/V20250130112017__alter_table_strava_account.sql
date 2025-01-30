19acc2b5364cdb578de634eb8ef79e81793a6da5
ngdangkietse <kietnguyen17052001@gmail.com>
2025-01-30 11:41:36 +0700

    feat(integration): alter table strava account

---
+++ migration/integration/V20250130112017__alter_table_strava_account.sql
@@ -0,0 +1,5 @@
+alter table strava_account
+    add column profile    varchar(255),
+    add column username   varchar(255),
+    add column first_name varchar(255),
+    add column last_name  varchar(255);
